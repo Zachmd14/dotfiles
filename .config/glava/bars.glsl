7c7546607c4b1b4033c0c376b3b7dc7cfffe38ba
Zachary <zacharymd14@gmail.com>
2024-12-06 12:36:57 +0100

    added glava visualizer

--- .config/glava/bars.glsl
+++ .config/glava/bars.glsl
@@ -2,27 +2,27 @@
 /* Center line thickness (pixels) */
 #define C_LINE 1
 /* Width (in pixels) of each bar */
-#define BAR_WIDTH 4
+#define BAR_WIDTH 50
 /* Width (in pixels) of each bar gap */
-#define BAR_GAP 2
+#define BAR_GAP 10
 /* Outline color */
 #define BAR_OUTLINE #262626
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
 #define BAR_OUTLINE_WIDTH 0
 /* Amplify magnitude of the results each bar displays */
-#define AMPLIFY 300
+#define AMPLIFY 600
 /* Whether the current settings use the alpha channel;
    enabling this is required for alpha to function
    correctly on X11 with `"native"` transparency. */
 #define USE_ALPHA 0
 /* How strong the gradient changes */
-#define GRADIENT_POWER 90
+#define GRADIENT_POWER 60
 /* Bar color changes with height */
 #define GRADIENT (d / GRADIENT_POWER + 1)
 /* Bar color */
-#define COLOR (#3366b2 * GRADIENT)
+#define COLOR (#e1e1e1)
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
-#define DIRECTION O
+#define DIRECTION 0
 /* Whether to switch left/right audio buffers */
 #define INVERT 0
 /* Whether to flip the output vertically */
