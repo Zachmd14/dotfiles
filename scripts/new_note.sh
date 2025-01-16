7a5d551e556f9397a632eacc0936b13927878133
Zach <zacharymd14@gmail.com>
2025-01-16 22:15:38 +0100

    added directory choice, but still dont work

--- scripts/new_note.sh
+++ scripts/new_note.sh
@@ -4,13 +4,38 @@
 read -p "Enter the note title: " note_title
 
 # Replace spaces with underscores and convert to lowercase for the filename
-file_name=$(echo "$note_title" | tr ' ' '_' | tr '[:upper:]' '[:lower:]').md
+file_name=$(echo $note_title | tr ' ' '-' | tr '[:upper:]' '[:lower:]').md
 
-# Specify the directory where you want to save the file
-directory="/home/zach/mainVault/02 - Zettelkasten/"
+# Define the 6 directories
+directories=(
+  "~/Documents/Obsidian/mainVault/1. Rough Notes/"
+  "~/Documents/Obsidian/mainVault/2. Ressources/"
+	"~/Documents/Obsidian/mainVault/3. Tags/"
+  "~/Documents/Obsidian/mainVault/4. Indexes/"
+  "~/Documents/Obsidian/mainVault/5. Full Notes/"
+  "~/Documents/Obsidian/mainVault/6. School/"
+)
 
-# Create the file with the given name in the specified directory
+# Display the options
+echo "Choose a directory to save the note:"
+for i in "${!directories[@]}"; do
+  echo "$((i + 1))) ${directories[$i]}"
+done
+
+# Ask the user to select a directory
+while true; do
+  read -p "Enter the number (1-6): " choice
+  if [[ "$choice" -ge 1 && "$choice" -le 6 ]]; then
+    directory="${directories[$((choice - 1))]}"
+    break
+  else
+    echo "Invalid choice. Please enter a number between 1 and 6."
+  fi
+done
+
+# Create the file with the given name in the chosen directory
 touch "$directory/$file_name"
 
 # Open the new file in neovim
 nvim "$directory/$file_name"
+
