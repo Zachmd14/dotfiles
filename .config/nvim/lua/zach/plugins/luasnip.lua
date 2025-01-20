0690eba551ddc51f0b1370c67135c3face4fe441
Zach <zacharymd14@gmail.com>
2025-01-20 08:55:06 +0100

    rewrited

--- .config/nvim/lua/zach/plugins/luasnip.lua
+++ .config/nvim/lua/zach/plugins/luasnip.lua
@@ -7,68 +7,54 @@ return {
 		local i = ls.insert_node
 		local f = ls.function_node
 
-		-- Define snippets for LaTeX
+		-- Snippets LaTeX (non-auto)
+		ls.add_snippets("tex", {
+			-- sous-section
+			s("sub", { t("\\subsection{"), i(1, "title"), t("}") }),
+
+			-- section
+			s("sec", { t("\\section{"), i(1, "title"), t("}") }),
+
+			-- nouvelle page
+			s("page", { t("\\newpage") }),
+		})
+
+		-- Snippets LaTeX (auto)
 		ls.add_snippets("tex", {
 			-- cube
 			s({ trig = "([A-Za-z%d]+)cb", regTrig = true, snippetType = "autosnippet" }, {
 				f(function(_, snip)
-					local var = snip.captures[1] -- Capture the letter or number before 'sr'
-					return var .. "^3"      -- Format as 'a^2' or '3^2'
+					local var = snip.captures[1]
+					return var .. "^3"
 				end)
 			}),
 
-			-- square root
-			s("sqrt", {
-				t("\\sqrt")
-			}),
+			-- racine carrée
+			s("sqrt", { t("\\sqrt") }),
 
-			-- subsection
-			s("sub", {
-				t("\\subsection{"), i(1, "title"), t("}")
-			}),
+			-- texte en gras
+			s("bf", { t("\\textbf{"), i(1, "text"), t("}") }),
 
-			-- sectioon
-			s("sec", {
-				t("\\section{"), i(1, "title"), t("}")
-			}),
+			-- opérateurs mathématiques
+			s("geq", { t("\\geq") }),
+			s("leq", { t("\\leq") }),
 
-			-- bold text
-			s("bf", {
-				t("\\textbf{"), i(1, "text"), t("}")
-			}),
-
-			-- greater than or equal to
-			s("geq", {
-				t("\\geq")
-			}),
-
-			-- less than or equal to
-			s("leq", {
-				t("\\leq")
-			}),
-
-			-- .10^
+			-- 10^
 			s({ trig = "([A-Za-z%d]+),,", regTrig = true, snippetType = "autosnippet" }, {
 				f(function(_, snip)
-					local var = snip.captures[1] -- Capture the letter or number before 'sr'
-					return var .. ".10^"    -- Format as 'a^2' or '3^2'
+					local var = snip.captures[1]
+					return var .. ".10^"
 				end),
 				i(1, "number")
 			}),
 
-			-- New page
-			s("page", {
-				t("\\newpage")
-			}),
-
-			-- Begin and End Environment
+			-- environnement
 			s("beg", {
-				t("\\begin{"), i(1, "environment"), t({ "}", "\t" }),
-				i(0),
-				t({ "", "\\end{" }), f(function(args) return args[1][1] end, { 1 }), t("}"),
+				t("\\begin{"), i(1, "environment"), t("}"),
+				i(0), t({ "", "\\end{" }), f(function(args) return args[1][1] end, { 1 }), t("}")
 			}),
 
-			-- Inline Math
+			-- Mathématiques en ligne
 			s("mk", {
 				t("$"), i(1), t("$"), i(2),
 				f(function(_, snip)
@@ -82,129 +68,79 @@ return {
 				i(0),
 			}),
 
-			-- Display Math
+			-- Mathématiques affichées
 			s("dm", {
-				t("\\["),
-				t({ "", "\t" }), i(1), t({ "", "\\]" }),
-				t({ "", "\t" }), i(2),
+				t({ "\\[", "" }),
+				t("  "), i(1),
+				t({ "", "\\]" })
 			}),
 
-			-- Summation
-			s("+++", {
-				t("\\sum_{"), i(1, "first"), t("}^{"), i(2, "max"), t("}{"), i(3, "formula"), t("}")
-			}),
+			-- Somme
+			s("+++", { t("\\sum_{"), i(1, "first"), t("}^{"), i(2, "max"), t("}{"), i(3, "formula"), t("}") }),
 
-			-- Limit
-			s("lm", {
-				t("\\lim_{"), i(1, "set"), t(" \\to "), i(2, "plus or less"), t("\\infty}")
-			}),
+			-- Limite
+			s("lm", { t("\\lim_{"), i(1, "set"), t(" \\to "), i(2, "plus ou moins"), t("\\infty}") }),
 
-			-- Subscript
-			s("_", {
-				i(1, "letter"), t("_{"), i(2, "number"), t("}"), i(3)
-			}),
+			-- Indice
+			s("_", { t("_{"), i(1, "number"), t("}") }),
 
-			-- Superscript (Autosnippet)
+			-- Exposant
 			s({ trig = "([A-Za-z%d]+)sr", regTrig = true, snippetType = "autosnippet" }, {
 				f(function(_, snip)
-					local var = snip.captures[1] -- Capture the letter or number before 'sr'
-					return var .. "^2"      -- Format as 'a^2' or '3^2'
+					local var = snip.captures[1]
+					return var .. "^2"
 				end)
 			}),
 
-			-- Text
-			s("tx", {
-				t("\\text{"), i(1, "text"), t("}")
-			}),
+			-- Texte
+			s("tx", { t("\\text{"), i(1, "text"), t("}") }),
 
 			-- Fraction
 			s({ trig = "//", regTrig = true, snippetType = "autosnippet" }, {
 				t("\\frac{"), i(1, "numerator"), t("}{"), i(2, "denominator"), t("}")
 			}),
 
-			-- For All
+			-- Pour tout
 			s({ trig = "fall", regTrig = true, snippetType = "autosnippet" }, {
 				t("\\forall "), i(1, "letter"), t(" \\in \\mathbb{"), i(2, "set"), t("}")
 			}),
 
 		}, { type = "autosnippets", key = "all_auto" })
 
-		-- Define snippets for Markdown
+		-- Snippets Markdown
 		ls.add_snippets("markdown", {
-			-- Begin and End Environment
-			s("beg", {
-				t("\\begin{"), i(1, "environment"), t({ "}", "\t" }),
-				i(0),
-				t({ "", "\\end{" }), f(function(args) return args[1][1] end, { 1 }), t("}"),
-			}),
+			-- texte en gras
+			s("bold", { t("**"), i(1), t("**"), i(2) }),
 
-			-- bold text
-			s("bold", {
-				t("**"), i(1), t("**"), i(2)
-			}),
+			-- callout important
+			s("imp", { t("> [!important]"), t({ "", "" }), t("> "), i(1) }),
 
-			-- important callout
-			s("imp", {
-				t("> [!important]"),
-				t({ "", "" }),
-				t("> "), i(1)
-			}),
+			-- callout tip
+			s("tip", { t("> [!tip]"), t({ "", "" }), t("> "), i(1) }),
 
-			-- tip callout
-			s("tip", {
-				t("> [!tip]"),
-				t({ "", "" }),
-				t("> "), i(1)
-			}),
+			-- date
+			s("date", { f(function() return os.date("%Y-%m-%d") end) }),
 
-			-- Date
-			s("date", {
-				f(function()
-					return os.date("%Y-%m-%d") -- Formats the date as YYYY-MM-DD
-				end),
-			}),
+			-- boîte
+			s("tbox", { t("\\fbox{"), i(1, "text"), t("}") }),
 
-			-- Box
-			s("tbox", {
-				t("\\fbox{"), i(1, "text"), t("}"),
-			}),
+			s("fbox", { t("\\boxed{"), i(1, "formula"), t("}") }),
 
-			s("fbox", {
-				t("\\boxed{"), i(1, "formula"), t("}"),
-			})
+			-- espace insécable
+			s("nbsp", { t("&nbsp;") }),
 
-		})
-
-		ls.add_snippets("markdown", {
-			-- cube
-			s({ trig = "([A-Za-z%d]+)cb", regTrig = true, snippetType = "autosnippet" }, {
-				f(function(_, snip)
-					local var = snip.captures[1] -- Capture the letter or number before 'sr'
-					return var .. "^3"      -- Format as 'a^2' or '3^2'
-				end)
-			}),
-
-			-- square root
-			s("sqrt", {
-				t("\\sqrt")
-			}),
-
-			-- adaptive parenthesis
+			-- parenthèses adaptatives
 			s({ trig = "lrp", regTrig = true, snippetType = "autosnippet" }, {
 				t("\\left("), i(1), t("\\right)")
 			}),
 
-			-- left / right arrow
-			s("rlarrow", {
-				t("\\leftrightarrows")
-			}),
+			-- flèche gauche-droite
+			s("rlarrow", { t("\\leftrightarrows") }),
 
-			-- left right arrow
-			s("Lrarrow", {
-				t("\\Leftrightarrow")
-			}),
+			-- flèche gauche-droite (double)
+			s("Lrarrow", { t("\\Leftrightarrow") }),
 
-			-- Inline Math
+			-- Mathématiques en ligne
 			s("mk", {
 				t("$"), i(1), t("$"), i(2),
 				f(function(_, snip)
@@ -218,78 +154,47 @@ return {
 				i(0),
 			}),
 
-			-- Display Math
-			s("dm", {
-				t("$$"),
-				t({ "", "\t" }), i(1), t({ "", "$$" }),
-				t({ "", "\t" }), i(2),
-			}),
+			-- Mathématiques affichées
+			s("dm", { t("$$"), i(1), t("$$") }),
 
-			-- Summation
-			s("+++", {
-				t("\\sum_{"), i(1, "first"), t("}^{"), i(2, "max"), t("}{"), i(3, "formula"), t("}")
-			}),
+			-- Somme
+			s("+++", { t("\\sum_{"), i(1, "first"), t("}^{"), i(2, "max"), t("}{"), i(3, "formula"), t("}") }),
 
-			-- Limit
-			s("lm", {
-				t("\\lim_{"), i(1, "set"), t(" \\to "), i(2, "plus or less"), t("\\infty}")
-			}),
+			-- Limite
+			s("lm", { t("\\lim_{"), i(1, "set"), t(" \\to "), i(2, "plus ou moins"), t("\\infty}") }),
 
-			-- Subscript
-			s("_", {
-				t("_{"), i(1, "number"), t("}"),
-			}),
+			-- Indice
+			s("_", { t("_{"), i(1, "number"), t("}") }),
 
-			-- Superscript (Autosnippet)
+			-- Exposant
 			s({ trig = "([A-Za-z%d]+)sr", regTrig = true, snippetType = "autosnippet" }, {
 				f(function(_, snip)
-					local var = snip.captures[1] -- Capture the letter or number before 'sr'
-					return var .. "^2"      -- Format as 'a^2' or '3^2'
+					local var = snip.captures[1]
+					return var .. "^2"
 				end)
 			}),
 
-			-- Text
-			s("tx", {
-				t("\\text{"), i(1, "text"), t("}")
-			}),
+			-- Texte
+			s("tx", { t("\\text{"), i(1, "text"), t("}") }),
 
 			-- Fraction
 			s({ trig = "//", regTrig = true, snippetType = "autosnippet" }, {
 				t("\\frac{"), i(1, "numerator"), t("}{"), i(2, "denominator"), t("}")
 			}),
 
-			-- For All
+			-- Pour tout
 			s({ trig = "fall", regTrig = true, snippetType = "autosnippet" }, {
 				t("\\forall "), i(1, "letter"), t(" \\in \\mathbb{"), i(2, "set"), t("}")
 			}),
 
-			-- greater than or equal to
-			s("geq", {
-				t("\\geq")
-			}),
+		})
 
-			-- less than or equal to
-			s("leq", {
-				t("\\leq")
-			}),
-
-			-- .10^
-			s({ trig = "([A-Za-z%d]+),,", regTrig = true, snippetType = "autosnippet" }, {
-				f(function(_, snip)
-					local var = snip.captures[1] -- Capture the letter or number before 'sr'
-					return var .. ".10^"    -- Format as 'a^2' or '3^2'
-				end),
-				i(1, "number")
-			}),
-
-		}, { type = "autosnippets", key = "all_auto" })
-
-		-- Enable autosnippets in your configuration
+		-- Configuration de LuaSnip
 		ls.config.set_config({
 			enable_autosnippets = true,
 		})
 
-		-- Key mappings for jumping between snippets
+		-- Mappage des touches
 		vim.api.nvim_set_keymap("i", "<Tab>", "<cmd>lua require'luasnip'.jump(1)<Cr>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("i", "<s-Tab>", "<cmd>lua require'luasnip'.jump(-1)<Cr>", { noremap = true, silent = true })
 	end
