0664913e243efd2de760baefc2e9dc98538e9f25
Zachmd14 <zacharymd14@gmail.com>
2025-04-17 02:01:17 +0200

    minor

--- .config/nvim/lua/zach/plugins/codeium.lua
+++ .config/nvim/lua/zach/plugins/codeium.lua
@@ -5,8 +5,9 @@ return {
 	config = function()
 		local neocodeium = require("neocodeium")
 		neocodeium.setup()
-		vim.keymap.set("i", "<A-CR>", neocodeium.accept)
-		vim.keymap.set("i", "<S-CR>", neocodeium.accept_word)
-		vim.keymap.set("i", "<A-Tab>", neocodeium.cycle)
+		vim.
+		vim.keymap.set("i", "<A-Tab>", neocodeium.accept)
+		vim.keymap.set("i", "<C-Tab>", neocodeium.accept_word)
+		vim.keymap.set("i", "<S-Tab>", neocodeium.cycle)
 	end,
 }
